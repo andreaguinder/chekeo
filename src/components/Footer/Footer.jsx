20255c2ec12f37136279f4eebe07c9ade74b5765
Andy Guinder <andreabelen.guinder@gmail.com>
2026-07-10 19:42:47 -0300

    acomodo estilos del footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.scss';
 const Footer = () => {
 
     return (
-        <footer>
+        <footer className={styles.footer}>
         <h2>Desarrollado por <a href="https://andreaguinder.com/">Andrea Guinder</a></h2>
         </footer>
     )
