d37f58c8d196baa002043c3a30b22eb3fc418071
Andy Guinder <andreabelen.guinder@gmail.com>
2026-07-11 03:56:11 -0300

    mejoro la ui

--- src/data/mock.js
+++ src/data/mock.js
@@ -1,14 +1,14 @@
 const tareas = [
   {
     id: 1,
-    title: "Esta es una tarea de ejemplo Pendiente",
-    descripcion: "Podés eliminarla (con el botón de tacho de basura) o marcarla como Completada (con el botón de check)",
+    title: "Esta es una tarea de ejemplo pendiente",
+    descripcion: "Podés eliminarla (con el botón de tacho de basura) o marcarla como completada (con el botón de check)",
     status: false
   },
   {
     id: 2,
-    title: "Esta es una tarea de ejemplo Completada",
-    descripcion: "Podés eliminarla (con el botón de tacho de basura), o volverla a la lista de Pendientes (con el botón del reloj)",
+    title: "Esta es una tarea de ejemplo completada",
+    descripcion: "Podés eliminarla (con el botón de tacho de basura), o volverla a la lista de pendientes (con el botón del reloj)",
     status: true
   }
 ]
