f69f31d21bf2403718e56527f7fb2e20a34c8c0e
Andy Guinder <andreabelen.guinder@gmail.com>
2026-08-20 19:38:51 -0300

    sigo intentando que funcione

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,22 +1,20 @@
-// src/config/firebase.js
+// config/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  // Si está en producción, usar el dominio actual del sitio para evitar choque de dominios
+  authDomain: window.location.hostname.includes("localhost")
+    ? import.meta.env.VITE_FIREBASE_AUTH_DOMAIN
+    : window.location.hostname,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
-
-// Exportamos lo que necesitamos
 export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
